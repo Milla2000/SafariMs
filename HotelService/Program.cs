4e1ac134d295d2d5cf1cc9d0e15dc535569fa8b9
Milla Jesso <milla.jesso1@accesscfa.com>
2024-09-14 22:30:05 +0300

    added cors policy to ToursService

--- HotelService/Program.cs
+++ HotelService/Program.cs
@@ -38,15 +38,27 @@ builder.Logging.AddConsole();
 
 builder.Services.AddHttpClient("Tours", c => c.BaseAddress = new Uri(builder.Configuration.GetValue<string>("ServiceURl:TourService")));
 
-var serviceUri = builder.Configuration.GetValue<string>("ServiceURl:TourService");
-Console.WriteLine($"ServiceURl:TourService: {serviceUri}");
-
-if (string.IsNullOrEmpty(serviceUri))
+builder.Services.AddCors(options =>
 {
-    // Log error and stop the application
-    Console.WriteLine("Error: ServiceURl:TourService configuration is missing.");
-    Environment.Exit(1); // Exit with an error code
-}
+    options.AddPolicy("HotelPolicy", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200");
+        // Optionally allow specific methods (GET, POST, etc.)
+        policy.WithMethods("GET", "POST", "PUT", "DELETE");
+        // Optionally allow specific headers
+        policy.WithHeaders("Content-Type", "Authorization");
+    });
+});
+
+//var serviceUri = builder.Configuration.GetValue<string>("ServiceURl:TourService");
+//Console.WriteLine($"ServiceURl:TourService: {serviceUri}");
+
+//if (string.IsNullOrEmpty(serviceUri))
+//{
+//    // Log error and stop the application
+//    Console.WriteLine("Error: ServiceURl:TourService configuration is missing.");
+//    Environment.Exit(1); // Exit with an error code
+//}
 
 //this is registering the service used in the controller for dependency injection
 builder.Services.AddScoped<ITour, ToursServvices>();
@@ -62,6 +74,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseCors("HotelPolicy");
 
 app.UseMigrations();
 
